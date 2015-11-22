44f4636e76df263ee168ab1502774c442d107959
stoffer6740 <cbj@c-bjerregaard.dk>
2015-11-22 11:07:41 +0100

    Initial commit

---
+++ GOTO-Night/GOTO-Night/www/scripts/camera.js
@@ -0,0 +1,23 @@
+﻿
+
+function capturePhoto() {
+
+    var cameraOptions = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    }
+
+    navigator.camera.getPicture(CameraSuccess, CameraError, cameraOptions);
+}
+
+function CameraSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function CameraError(error) {
+    alert("Something went wrong");
+    console.log(error);
+
+}
+
